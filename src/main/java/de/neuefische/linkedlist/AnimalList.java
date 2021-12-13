f3ee0fbaba51b07829f71932732891441834c8d9
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-12-13 09:56:05 +0100

    Add AnimalList

---
+++ src/main/java/de/neuefische/linkedlist/AnimalList.java
@@ -0,0 +1,23 @@
+package de.neuefische.linkedlist;
+
+public class AnimalList {
+    private AnimalListItem head;
+
+
+    public void add(Animal animal) {
+        if (head == null) {
+            head = new AnimalListItem(animal);
+        }
+        else {
+            AnimalListItem current = head;
+
+            while(current.getNext() != null) {
+                current = current.getNext();
+            }
+
+            current.setNext(new AnimalListItem(animal));
+        }
+    }
+
+
+}
